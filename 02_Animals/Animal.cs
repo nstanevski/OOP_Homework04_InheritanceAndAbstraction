5eea24341e893c76ab9b81282f985483d5ac88bc
nstanevski <nikolay.stanevski@gmail.com>
2015-06-09 10:26:50 +0300

    task#2 - classes added

--- 02_Animals/Animal.cs
+++ 02_Animals/Animal.cs
@@ -6,8 +6,68 @@ using System.Threading.Tasks;
 
 namespace _02_Animals
 {
+    public enum Gend { Female, Male};
+
     public abstract class Animal:ISoundProducible
     {
+        private string name;
+        private int age;
+        private Gend gender;
+
+        public Animal(string name, int age, Gend gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException(
+                        "Name cannot be null, empty or whitespace only"
+                        );
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 0 || value > 30)
+                    throw new ArgumentOutOfRangeException("Age is a number in range [1..30]");
+
+            }
+        }
+
+        public Gend Gender
+        {
+            get
+            {
+                return this.gender;
+            }
+            set
+            {
+                if ((int)value < 0 || (int)value > 1)
+                    throw new ArgumentOutOfRangeException(
+                        "Gender can be either 0 or 1 (female/male)");
+                this.gender = value;
+            }
+        }
+
+
+
         public abstract void ProduceSound();
     }
 }
